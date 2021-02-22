5b6521cf1afcdd206ab8d1ccf87aafdc53e252de
Doktan <thekonor@gmail.com>
2021-02-22 17:27:29 +0300

    2nd Practical Task
    Second practical task

---
+++ 2pr/TestingPr2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingPr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert X");
+            int X;
+            if (!Int32.TryParse(Console.ReadLine(), out X))
+            {
+                Console.WriteLine("Input Error");
+            }
+            else
+            {
+                if (X > 0 && X <= 6)
+                {
+                    Console.WriteLine(5 * X - 1);
+                }
+                else
+                {
+                    if (X >= 10 && X < 12)
+                    {
+                        Console.WriteLine(2 * X * X + X + 1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Input Error");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
