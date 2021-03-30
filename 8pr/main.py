47c60fffed951a9be3b38758a5baf8ec5ca3d2b3
Doktan <thekonor@gmail.com>
2021-03-30 16:04:37 +0300

    alt ver of pr8
    added some new error handlers

--- 8pr/main.py
+++ 8pr/main.py
@@ -30,29 +30,51 @@ def main(arg):
         f = open('res.txt', 'w', encoding='utf-8')
         i = 0
         links = soup.find_all('a')
-        while i != 5:
-            r = requests.get(links[i].get('href')) #get request to link
-            innerSoup = BeautifulSoup(r.text, 'html.parser')
-            h1Tags = innerSoup.find_all('h1')
-            f.write('Link: ')
-            f.write(links[i].get('href'))
-            f.write('\n')
-
-            if len(h1Tags) == 0:
-                f.write('There is no any h1 tags\n')
-            else:
-                H1Count = 0
-                for tags in h1Tags:
-                    f.write('<h1> ')
-                    f.write(str(tags.getText()))
+        if len(links) == 0:
+            f.write('There is no any links in %s' %  url)
+        else:
+            for link in links:
+                error = None
+                try:
+                    r = requests.get(link.get('href')) #get request to link
+                except requests.exceptions.SSLError as e:
+                   error = "SSL Error %s\n" % e
+                   pass
+                except requests.exceptions.MissingSchema as e:
+                   error = "Inccorect URL %s\n" % link.get('href')
+                   pass
+                except (requests.exceptions.ConnectionError, requests.exceptions.ConnectTimeout) as e:
+                   error = "URL: %s unavailable!\n" % link.get('href')
+                   pass
+                except requests.exceptions.InvalidSchema as e:
+                    error = e
+                    pass
+                innerSoup = BeautifulSoup(r.text, 'html.parser')
+                h1Tags = innerSoup.find_all('h1')
+                f.write('Link: ')
+                f.write(str(link.get('href')))
+                f.write('\n')
+                if error is not None:
+                    f.write(error)
                     f.write('\n')
-                    H1Count += 1
-                    if H1Count != 3:
-                        break
-
-            i+=1
-            f.write('\n')
-
+                else:
+                    if len(h1Tags) == 0:
+                        f.write('There is no any h1 tags\n')
+                    else:
+                        H1Count = 0
+                        for tags in h1Tags:
+                            f.write('<h1> ')
+                            f.write(str(tags.getText()))
+                            f.write('\n')
+                            H1Count += 1
+                            if H1Count == 3:
+                                break
+                    f.write('\n')
+                i += 1
+                if i > 4:
+                    break
         f.close()
 
         return('Jobs done! Check res.txt')
+
+print(main('http://yandex.ru/'))
